a4e21c7781978a4a8412c6303d6e46845c367913
oghomwen <osazuwaoghomwen@gmail.com>
2022-04-24 19:18:39 +0200

    pages updated

--- asset/js/scr.js
+++ asset/js/scr.js
@@ -35,5 +35,23 @@ function openPage(pageName, elmnt, color) {
   
   document.getElementById("defaultOpen").click();
   
-
+var i = 0;
+function progress() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("progressBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width  + "%";
+      }
+    }
+  }
+}
 
